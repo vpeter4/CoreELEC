518aa88a806d4f25fc73ba4a9309273f0ebd7a24
Peter <peter.vicman@gmail.com>
2020-07-11 14:02:17 +0200

    CoreELEC 10: packages common to both devices

---
+++ projects/Amlogic-ce/packages/common/CoreELEC-settings/package.mk
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="CoreELEC-settings"
+PKG_VERSION="kodi19-cfaa7b1173df350af644c55c5f137209faca7860"
+PKG_SHA256=""
+PKG_LICENSE="GPL"
+PKG_SITE="https://coreelec.org"
+#PKG_URL="https://github.com/CoreELEC/service.coreelec.settings/archive/$PKG_VERSION.tar.gz"
+PKG_URL="file:///home/pvic/scratch/service.coreelec.settings-$PKG_VERSION.tar.gz"
+GET_HANDLER_SUPPORT="file"
+PKG_DEPENDS_TARGET="toolchain Python3 connman pygobject dbus-python"
+PKG_LONGDESC="CoreELEC-settings: is a settings dialog for CoreELEC"
+
+PKG_MAKE_OPTS_TARGET="DISTRONAME=$DISTRONAME ROOT_PASSWORD=$ROOT_PASSWORD"
+
+if [ "$DISPLAYSERVER" = "x11" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET setxkbmap"
+else
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bkeymaps"
+fi
+
+post_makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/coreelec
+    cp $PKG_DIR/scripts/* $INSTALL/usr/lib/coreelec
+
+  ADDON_INSTALL_DIR=$INSTALL/usr/share/kodi/addons/service.coreelec.settings
+
+  python_compile $ADDON_INSTALL_DIR/resources/lib/
+
+  python_compile $ADDON_INSTALL_DIR/defaults.py
+
+  python_compile $ADDON_INSTALL_DIR/oe.py
+}
+
+post_install() {
+  enable_service backup-restore.service
+  enable_service factory-reset.service
+}
---
+++ projects/Amlogic-ce/packages/common/remote/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="remote"
+PKG_VERSION="1"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.openelec.tv"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain eventlircd libirman v4l-utils evrepeat"
+PKG_SECTION="virtual"
+PKG_LONGDESC="Meta package for installing various tools needed for remote support"
+
+if [ "$AMREMOTE_SUPPORT" = "yes" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET amremote"
+fi
