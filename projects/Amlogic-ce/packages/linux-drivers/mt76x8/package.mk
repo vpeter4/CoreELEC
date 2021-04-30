b0f08e28afda72d27b5bcaeb4496a9fb31671c9e
Peter <peter.vicman@gmail.com>
2021-04-30 10:14:59 +0200

    driver mt76x8

--- projects/Amlogic-ce/packages/linux-drivers/mt76x8/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/mt76x8/package.mk
@@ -1,16 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2018-present Team CoreELEC (https://coreelec.tv)
 
-PKG_NAME="mt7668-aml"
-PKG_VERSION="fd76e10404b220c552fc7951f5a359b56d599e3b"
-PKG_SHA256="1fb62a608b9ce8ba8157a8025ba0d76ad1d085510b27982e4af33575d2841308"
+PKG_NAME="mt76x8"
+PKG_VERSION="b95205b6b4736d3d83c94b9c967d2b84f43210a0"
+PKG_SHA256="6f727c35bb0c2681d897f13fb9d0a8eecb0a20f10abcf1323c6beaee899f3f70"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/khadas/android_hardware_wifi_mtk_drivers_mt7668"
-PKG_URL="https://github.com/khadas/android_hardware_wifi_mtk_drivers_mt7668/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/CoreELEC/mt76x8"
+PKG_URL="https://github.com/CoreELEC/mt76x8/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_LONGDESC="mt7668 Linux driver"
+PKG_LONGDESC="mt76x8 Linux driver"
 PKG_IS_KERNEL_PKG="yes"
 PKG_TOOLCHAIN="manual"
 
@@ -19,11 +19,21 @@ pre_make_target() {
 }
 
 make_target() {
+  echo
+  echo "making bluetooth"
   make EXTRA_CFLAGS="-w" \
-    KERNELDIR=$(kernel_path) \
+    LINUX_SRC=$(kernel_path) \
     ARCH=$TARGET_KERNEL_ARCH \
     CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
-    -f $PKG_BUILD/Makefile
+    -C bluetooth/sdio -f Makefile
+
+  echo
+  echo "making wlan"
+  make EXTRA_CFLAGS="-w" \
+    LINUX_SRC=$(kernel_path) \
+    ARCH=$TARGET_KERNEL_ARCH \
+    CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+    -C wlan -f Makefile
 }
 
 makeinstall_target() {
@@ -31,5 +41,5 @@ makeinstall_target() {
     find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
 
   mkdir -p $INSTALL/$(get_full_firmware_dir)
-    cp $PKG_BUILD/7668_firmware/* $INSTALL/$(get_full_firmware_dir)
+    cp $PKG_BUILD/wlan/firmware/* $INSTALL/$(get_full_firmware_dir)
 }
