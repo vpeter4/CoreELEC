10e0a408d40269137532452819cfda2f448b9f85
Peter <peter.vicman@gmail.com>
2022-04-01 05:32:31 +0200

    libbdplus: bump to 0.2.0

--- packages/multimedia/libbdplus/package.mk
+++ packages/multimedia/libbdplus/package.mk
@@ -4,6 +4,10 @@
 PKG_NAME="libbdplus"
 PKG_VERSION="0.1.2"
 PKG_SHA256="a631cae3cd34bf054db040b64edbfc8430936e762eb433b1789358ac3d3dc80a"
+
+PKG_VERSION="0.2.0"
+PKG_SHA256=""
+
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.videolan.org/developers/libbdplus.html"
 PKG_URL="http://download.videolan.org/pub/videolan/${PKG_NAME}/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
